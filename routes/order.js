a5186c98834a85d0ee501a6cdebc7460337ed453
AndriyFitak <116104837+AndriyFitak@users.noreply.github.com>
2023-08-09 13:19:21 +0200

    js

--- routes/order.js
+++ routes/order.js
@@ -4,6 +4,7 @@ import { getOrderList, orderCreate } from "../controllers/orders.js";
 const orderRouter = Router()
 
 orderRouter.post('/create', orderCreate)
+
 orderRouter.get('/list', getOrderList)
 
 export default orderRouter
